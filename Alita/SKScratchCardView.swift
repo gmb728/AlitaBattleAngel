c5376f7cc2bea56e3ff52996b8f323fd644f9901
Chang Sophia <you@example.com>
2019-02-19 11:38:28 +0800

    init

---
+++ Alita/SKScratchCardView.swift
@@ -0,0 +1,76 @@
+//
+//  SKScratchCardView.swift
+//  Alita
+//
+//  Created by Chang Sophia on 2/19/19.
+//  Copyright © 2019 Chang Sophia. All rights reserved.
+//
+
+import UIKit
+
+class SKScratchCardView:UIView {
+    private var coverView = UIView()
+    private var contentView = UIView()
+    private var contentMaskView = SKScratchCardMaskView()
+    
+    var strokeWidth:CGFloat = 0 {
+        didSet {
+            self.contentMaskView.strokeWidth = self.strokeWidth
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    func setupWith(coverView:UIView, contentView:UIView) {
+        // cover view
+        self.coverView = coverView
+        
+        // content view
+        self.contentView = contentView
+        
+        // maskView
+        self.contentMaskView.frame = coverView.frame
+        self.contentMaskView.backgroundColor = UIColor.clear
+        self.contentMaskView.strokeWidth = 40.0
+        
+        // addSubviews
+        addSubviewFullscreen(self.coverView)
+        addSubviewFullscreen(self.contentView)
+        addSubviewFullscreen(self.contentMaskView)
+        
+        // set mask
+        self.contentView.mask = self.contentMaskView
+        
+        // add gesture
+        setupPanGesture()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addSubviewFullscreen(_ subview: UIView) {
+        subview.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(subview)
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-(0)-[subview]-(0)-|",
+                                                      options: NSLayoutConstraint.FormatOptions(),
+                                                      metrics: nil,
+                                                      views: ["subview": subview]))
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-(0)-[subview]-(0)-|",
+                                                      options: NSLayoutConstraint.FormatOptions(),
+                                                      metrics: nil,
+                                                      views: ["subview": subview]))
+    }
+    
+    private func setupPanGesture() {
+        let panGesture = UIPanGestureRecognizer(target: contentMaskView, action: #selector(contentMaskView.panGestureRecognizer))
+        self.addGestureRecognizer(panGesture)
+    }
+    
+    public func clearCanvas() {
+        contentMaskView.clearCanvas()
+}
+}
--- Alita/ViewController.swift
+++ Alita/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var scratchCardView: SKScratchCardView?
 
+    @IBOutlet weak var alitaImageView: UIImageView!
+    @IBOutlet weak var bloodImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        setupView()
+
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    private func setupView() {
+        let imageView = UIImageView(image: UIImage(named: "alita-battle-angel-4k-5v-1280x2120"))
+        imageView.contentMode = .scaleAspectFill
+        let coverView = UIImageView(image: UIImage(named: "alita"))
+        coverView.contentMode = .scaleAspectFill
+        let screen = UIScreen.main.bounds
+        scratchCardView = SKScratchCardView(frame:screen)
+        scratchCardView!.setupWith(coverView: coverView, contentView: imageView)
+        view.addSubview(scratchCardView!)
+    }
+    
 
-
+    //clear paths
+    @objc private func clearButtonClickHandler() {
+         print("clear")
+        scratchCardView?.clearCanvas()
+    }
+    
+    
 }
 
